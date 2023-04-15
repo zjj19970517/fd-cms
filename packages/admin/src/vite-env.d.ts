f73f5411110584ad22ea14d43d18352eb1c56f72
张锦杰 <zhangjinjie.zjj@alibaba-inc.com>
2023-04-15 16:21:13 +0800

    feat(admin): 添加.env

--- packages/admin/src/vite-env.d.ts
+++ packages/admin/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_AMAP_KEY: string;
+  readonly VITE_AMAP_SECURITY_JS_CODE: string;
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
